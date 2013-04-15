b58b28bd189da6e3c76ba8cf942a977466cbd23c
jesperdahlstrom <jepped91@gmail.com>
2013-04-15 15:24:34 +0200

    Ölnyheter hämtar data. Fixat bugg med upprepade hämtningar.

--- src/se/mima/jeda/frsa/krogrunda/BeerNewsFrag.java
+++ src/se/mima/jeda/frsa/krogrunda/BeerNewsFrag.java
@@ -1,18 +1,109 @@
 package se.mima.jeda.frsa.krogrunda;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import se.mima.jeda.frsa.krogrunda.JSONparser.MyCallbackInterface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class BeerNewsFrag extends Fragment{
+public class BeerNewsFrag extends ListFragment implements OnItemClickListener,
+		MyCallbackInterface {
 
-public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		
-		View myFragmentView  = inflater.inflate(R.layout.beer_news, container, false);
-		
+	private static String url = "http://systembevakningsagenten.se/api/json/1.0/newProducts.json";
+
+	static String TAG_ITEMS = "items";
+	static String TAG_ID = "id";
+	static String TAG_NAME = "name";
+	static String TAG_SYSID = "sysid";
+	static String TAG_COUNTRY = "country";
+	static String TAG_ALC = "alcohol_vol";
+
+	private static String name;
+	private static String sysid;
+	private static String country;
+	private static String alc;
+
+	ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+	private ArrayList<String> drinks = new ArrayList<String>();
+
+	JSONArray list = null;
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View myFragmentView = inflater.inflate(R.layout.beer_news, container,
+				false);
+
+		if (getListAdapter() == null) {
+
+			JSONparser parser = new JSONparser(this);
+			parser.execute(url);
+		}
+
 		return myFragmentView;
 	}
-	
-} 
+
+	@Override
+	public void onRequestComplete(JSONObject result) {
+		JSONObject json = result;
+
+		try {
+
+			list = json.getJSONArray(TAG_ITEMS);
+			Log.e("onRequestComplete", "Fått JSONArray");
+
+			for (int i = 0; i < list.length(); i++) {
+
+				JSONObject c = list.getJSONObject(i);
+
+				drinks.add(c.getString(TAG_ID));
+
+				HashMap<String, String> info = new HashMap<String, String>();
+				info.put(TAG_NAME, c.getString(TAG_NAME));
+				info.put(TAG_SYSID, "Artikelnr: " + c.getString(TAG_SYSID));
+				info.put(TAG_COUNTRY, c.getString(TAG_COUNTRY));
+				info.put(TAG_ALC, c.getString(TAG_ALC) + "%");
+
+				data.add(info);
+
+				name = c.getString(TAG_NAME);
+				sysid = c.getString(TAG_SYSID);
+				country = c.getString(TAG_COUNTRY);
+				alc = c.getString(TAG_ALC);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		SimpleAdapter adapter = new SimpleAdapter(getActivity(), data,
+				android.R.layout.simple_list_item_2, new String[] { TAG_NAME,
+						TAG_SYSID, TAG_COUNTRY, TAG_ALC }, new int[] {
+						android.R.id.text1, android.R.id.text2 });
+
+		setListAdapter(adapter);
+
+		ListView lv = getListView();
+		lv.setOnItemClickListener(this);
+
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/se/mima/jeda/frsa/krogrunda/CitiesFrag.java
+++ src/se/mima/jeda/frsa/krogrunda/CitiesFrag.java
@@ -19,43 +19,45 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class CitiesFrag extends ListFragment implements OnItemClickListener, MyCallbackInterface{
-	
+public class CitiesFrag extends ListFragment implements OnItemClickListener,
+		MyCallbackInterface {
+
 	private static String url = "http://api.fatkoll.se/json/1.0/getCities.json?api_key=fd6950b1499b71037ec3c5a5e01081d6";
-	
+
 	static String TAG_LIST = "list";
-	static String TAG_ID ="id";
+	static String TAG_ID = "id";
 	static String TAG_NAME = "name";
-	
+
 	private ArrayList<String> cities = new ArrayList<String>();
 	private ArrayList<String> cityIds = new ArrayList<String>();
-	
+
 	JSONArray list = null;
-	
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		
-		View myFragmentView  = inflater.inflate(R.layout.cities, container, false);
-		
-		JSONparser parser = new JSONparser(this);		
-		parser.execute(url);
-		
-				
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View myFragmentView = inflater.inflate(R.layout.cities, container,
+				false);
+
+		if (getListAdapter() == null) {
+			JSONparser parser = new JSONparser(this);
+			parser.execute(url);
+		}
+
 		return myFragmentView;
 	}
-	
-	@Override
-	public void onRequestComplete(JSONObject result){
 
-		
+	@Override
+	public void onRequestComplete(JSONObject result) {
+
 		JSONObject json = result;
-		
+
 		try {
-			
+
 			list = json.getJSONArray(TAG_LIST);
 			Log.e("onRequestComplete", "Fått JSONArray");
-			
-			
-			for (int i = 0; i < list.length(); i++){
+
+			for (int i = 0; i < list.length(); i++) {
 				JSONObject c = list.getJSONObject(i);
 				Log.e("onRequestComplete", list.getString(i));
 				cityIds.add(c.getString(TAG_ID));
@@ -64,24 +66,26 @@ public class CitiesFrag extends ListFragment implements OnItemClickListener, MyC
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, cities);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, cities);
 		Log.e("onRequestComplete", "Skapar ArrayAdapter");
 		setListAdapter(adapter);
 		ListView lv = getListView();
 		lv.setOnItemClickListener(this);
 		Log.e("onRequestComplete", "Sätter ListAdapter");
-		
+
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
 		String cityId = cityIds.get(position).toString();
-		Intent startPubsListIntent = new Intent(getActivity(), PubsActivity.class);
+		Intent startPubsListIntent = new Intent(getActivity(),
+				PubsActivity.class);
 		startPubsListIntent.putExtra("cityId", cityId);
 		startActivity(startPubsListIntent);
-		
+
 	}
-	
-	
-} 
+
+}
